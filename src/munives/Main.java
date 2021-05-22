9a50377c5155c5bec363689cd24d461270d67163
Valeria Moreno <vale-riamo@hotmail.com>
2021-05-21 22:06:28 -0400

    Point

--- src/munives/Main.java
+++ src/munives/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Matías, my name is Valeria.");
+        System.out.println("Hello Matías, my name is Valeria");
     }
 }
